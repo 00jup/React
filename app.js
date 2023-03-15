1438eb6a551eed986c2818112d56815fa35d5803
박정욱 <parkjeong-uk@bagjeong-ug-ui-MacBookPro-16.local>
2023-03-15 14:28:04 +0900

    Modified app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-const loginFrom = document.querySelector("#login-form");
+//const loginFrom = document.querySelector("#login-form");
+const loginInput = loginFrom.querySelector("#login-form input");
+const loginButton = loginFrom.querySelector("#login-form button"); 
+
+
+function onLoginBtnClick() {}
+
+loginButton.addEventListener("click", onLoginBtnClick);
