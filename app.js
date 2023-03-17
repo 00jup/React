a3bee06f7e2cde06aa022380a3560712a3a8fa56
박정욱 <parkjeong-uk@Haeun.localdomain>
2023-03-17 12:07:02 +0900

    value.length in js

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-prompt("MODIFIED");
+alert("MODIFIED");
+loginInput.value = prompt("What is your name?", "name");
+const userName = loginInput.value;
 function onLoginBtnClick() {
   ///console.dir(loginInput);
-  console.log("Hello", loginInput.value);
+  //console.log("Hello", loginInput.value);
+  if (loginInput.value === "") {
+    alert("Please write your name");
+  } else if (loginInput.value.length > 15) {
+    alert("tooooo long");
+  } else {
+    alert("YOUR NAME IS " + loginInput.value);
+    console.log("Hello", loginInput.value);
+  }
 }
-
+s;
 loginButton.addEventListener("click", onLoginBtnClick);
