5109827e836c00ce41fe8adc7404b70a94332bcc
박정욱 <parkjeong-uk@Haeun.localdomain>
2023-03-17 11:02:23 +0900

    add prompt

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //const loginFrom = document.querySelector("#login-form");
-const loginInput = loginFrom.querySelector("#login-form input");
-const loginButton = loginFrom.querySelector("#login-form button"); 
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-
-function onLoginBtnClick() {}
+prompt("MODIFIED");
+function onLoginBtnClick() {
+  ///console.dir(loginInput);
+  console.log("Hello", loginInput.value);
+}
 
 loginButton.addEventListener("click", onLoginBtnClick);
