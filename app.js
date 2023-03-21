f9634b04c7bb5fe103b8f2c537a1364d70b42bfe
박정욱 <jupark0369@gmail.com>
2023-03-21 13:12:36 +0900

    Events part two

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
+const link = document.querySelector("a");
+
 //alert("MODIFIED");
 
 loginInput.value = prompt("What is your name?", "name");
@@ -14,12 +16,19 @@ function onLoginBtnClick(event) {
   if (loginInput.value === "") {
     alert("Please write your name");
   } else if (loginInput.value.length > 15) {
-    alert("tooooo long");
+    alert("tooo long");
   } else {
     alert("YOUR NAME IS " + loginInput.value);
     console.log("Hello", loginInput.value);
   }
 }
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.log(event);
+  alert("clecked!");
+}
 //loginForm.addEventListener("submit", onLoginBtnClick);
+//loginForm.addEventListener("submit", onLoginBtnClick() 넣으면 한번 실행되고 끝난다.);
 
 loginButton.addEventListener("click", onLoginBtnClick);
+link.addEventListener("click", handleLinkClick);
