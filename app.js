6c5cda3a27ff12b8aeeaa00fe6a18d99d3e512e2
박정욱 <jupark0369@gmail.com>
2023-03-21 15:39:44 +0900

    vanilla script lecture

--- app.js
+++ app.js
@@ -6,29 +6,58 @@ const link = document.querySelector("a");
 
 //alert("MODIFIED");
 
-loginInput.value = prompt("What is your name?", "name");
-const userName = loginInput.value;
+//loginInput.value = prompt("What is your name?", "name");
+//const userName = loginInput.value;
 function onLoginBtnClick(event) {
   ///console.dir(loginInput·‚·);
   //console.log("Hello", loginInput.value);
   event.preventDefault();
   console.log(loginInput.value);
   if (loginInput.value === "") {
-    alert("Please write your name");
+    //alert("Please write your name");
   } else if (loginInput.value.length > 15) {
-    alert("tooo long");
+    //alert("tooo long");
   } else {
-    alert("YOUR NAME IS " + loginInput.value);
-    console.log("Hello", loginInput.value);
+    //alert("YOUR NAME IS " + loginInput.value);
   }
 }
+
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+//string을 반복해서 사용하는 경우에 할 수 있는 것 변수로 만들어줘라. 그러면 오타가 나면 자바스크립트가 찾아준다.
+const USERNAME_KEY = "username";
+
 function handleLinkClick(event) {
   event.preventDefault();
   console.dir(event);
-  alert("clecked!");
+  alert("clicked!");
 }
 //loginForm.addEventListener("submit", onLoginBtnClick);
 //loginForm.addEventListener("submit", onLoginBtnClick() 넣으면 한번 실행되고 끝난다.);
 
-loginButton.addEventListener("click", onLoginBtnClick);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = "Hello " + username;
+  //prettier-ignore
+  localStorage.setItem(USERNAME_KEY, username);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+
+  console.log(username);
+}
+//loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${savedUsername}`;
+}
